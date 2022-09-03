50ed6579940b902a9b5b37681230d5a1f814dc20
haianhne <haianhinclude@gmail.com>
2022-09-03 09:38:31 +0700

    Clean code

--- foodordering/src/main/java/com/n37/foodordering/controllers/LoginController.java
+++ foodordering/src/main/java/com/n37/foodordering/controllers/LoginController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.n37.foodordering.model.User;
+
 @Controller
 public class LoginController {
 	@GetMapping("/login")
